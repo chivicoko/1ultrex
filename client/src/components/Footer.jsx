d245f56bdcf5de509fa85e11b2cb581f1561fce1
chivicoko <vicharmonic@gmail.com>
2023-05-11 00:37:28 +0100

    Adjusted Copyright statement

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -28,7 +28,7 @@ const Footer = () => {
                     <p>VIP Only</p>
                 </div>
             </div>
-            <p className='copyright'>Copyright &copy; <span style={{color: 'orange'}}>ultrexgold.com</span> 2023 <br />All Rights reserved</p>
+            <p className='copyright'>Copyright &copy; <span style={{color: 'orange'}}>UltrexGold Intl.</span> 2023 <br />All Rights reserved</p>
         </div>
     </footer>
   )
