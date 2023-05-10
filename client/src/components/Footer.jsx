a2fe29819b11f29b7e645d34e87af2fe95fae4a1
chivicoko <vicharmonic@gmail.com>
2023-05-11 00:42:08 +0100

    Adjusted the copyright...

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -28,7 +28,7 @@ const Footer = () => {
                     <p>VIP Only</p>
                 </div>
             </div>
-            <p className='copyright'>&copy; 2023 <span style={{color: 'orange'}}>UltrexGold Intl.</span> <br />All Rights reserved</p>
+            <p className='copyright'>&copy; 2023 <span style={{color: 'orange'}}>UltrexGold Intl.</span> All Rights reserved</p>
         </div>
     </footer>
   )
