c8de41d7e10c8f8eb1f639db718bf1f3f4694f0d
chivicoko <vicharmonic@gmail.com>
2023-05-29 17:13:19 +0100

    Adjusted user dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -66,16 +66,16 @@ const Dashboard = () => {
         <section>
           <div className='userContainer'>
             <div className='userSection' style={{backgroundColor: 'grey', padding: '25px', fontSize: '15px', margin: '30px 5px', borderRadius: '15px'}}>
-              {/* <h1 style={{textAlign: 'center', marginTop: '10px', marginBottom: '25px', color: 'orange'}}>Your Account</h1> */}
-              <div style={{marginBottom: '20px', display: 'flex', justifyContent: "space-between", alignItems: "start"}}><p>{t('Username')}:</p><p>{user.username}</p></div>
+              {/* <div style={{marginBottom: '20px', display: 'flex', justifyContent: "space-between", alignItems: "start"}}><p>{t('Username')}:</p><p>{user.username}</p></div>
               <div style={{marginBottom: '20px', display: 'flex', justifyContent: "space-between", alignItems: "start"}}><p>{t('Registration Date')}:</p><p style={{fontSize: '13px', marginTop: '3px'}}>{new Date(user.createdAt).toLocaleString('en-US')}</p></div>
-              {/* <div style={{marginBottom: '20px', display: 'flex', justifyContent: "space-between", alignItems: "start"}}><p>{t('Last Access')}:</p><p>{user.createdAt}</p></div> */}
               <div style={{marginBottom: '20px', display: 'flex', justifyContent: "space-between", alignItems: "start"}}><p>{t('Account Balance')}:</p><p>$0.00</p></div>
               <div style={{marginBottom: '20px', display: 'flex', justifyContent: "space-between", alignItems: "start"}}><p>{t('Earned Total')}:</p><p>$0.00</p></div>
               <div style={{marginBottom: '20px', display: 'flex', justifyContent: "space-between", alignItems: "start"}}><p>{t('Pending Withdrawal')}:</p><p>$0.00</p></div>
               <div style={{marginBottom: '20px', display: 'flex', justifyContent: "space-between", alignItems: "start"}}><p>{t('Withdrew Total')}:</p><p>$0.00</p></div>
-              <div style={{display: 'flex', justifyContent: "space-between", alignItems: "start"}}><p>{t('Active Deposite')}:</p><p>$0.00</p></div>
+              <div style={{display: 'flex', justifyContent: "space-between", alignItems: "start"}}><p>{t('Active Deposite')}:</p><p>$0.00</p></div> */}
           </div>
+            
+            <h3 style={{marginBottom: '0'}}>Use the navigation bars above to navigate through your dashboard</h3>
 
           </div>
         </section>
